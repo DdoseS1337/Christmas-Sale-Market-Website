e06cf9efb2b55ff59ca2112032ffb0cf7b3c7a5b
GineTik <densheva18072003@gmail.com>
2023-10-10 20:32:18 +0300

    Moved christmas-text style

--- frontend/src/components/Header/HeaderLogo.tsx
+++ frontend/src/components/Header/HeaderLogo.tsx
@@ -2,7 +2,7 @@ const HeaderLogo = () => {
     return (
         <div className="d-flex">
             <div className="logo-icon"></div>
-            <h2 className="align-self-center" style={{ margin: 0, padding: 0 }}>
+            <h2 className="align-self-center christmas-text" style={{ margin: 0, padding: 0 }}>
                 Christmas Market
             </h2>
         </div>
