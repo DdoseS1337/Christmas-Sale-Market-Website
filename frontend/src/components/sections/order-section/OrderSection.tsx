35c3276aa76af4b3152df4bf839624fcca64ab1c
Oleksii Butko <alexbuttko@gmail.xom>
2023-10-18 22:12:02 +0300

    Create a new ordersection

---
+++ frontend/src/components/sections/order-section/OrderSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section } from '../../common/Section'
+import "../../../styles/components/sections/order-section/order-section.css"
+
+export const OrderSection = () => {
+  return (
+    <Section>
+        <h2 className='Biling'>Biling Information</h2>
+        <input/>
+    </Section>
+  )
+}
