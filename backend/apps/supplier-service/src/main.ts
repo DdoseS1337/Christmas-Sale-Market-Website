4c61d7b86696cc640e2d7025a90f84ece6401b29
Yaroslav Maksymchuk <yaroslav.maksymchuk@gmail.com>
2023-11-12 21:00:27 +0200

    small fix cookies

--- backend/apps/supplier-service/src/main.ts
+++ backend/apps/supplier-service/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SupplierModule } from './supplier.module';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(SupplierModule);
-  app.useLogger(app.get(Logger))
   const configService = app.get(ConfigService);
+  app.useLogger(app.get(Logger))
   app.enableCors();
+  app.use(cookieParser());
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
