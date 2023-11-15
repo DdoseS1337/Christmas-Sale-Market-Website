e53c8c49d20784dcd98a1281edc381883de574c9
Pier228 <artem.buxanenko@gmail.com>
2023-11-15 19:50:41 +0200

    Add adaptivity to Basket page and Contacts page. Replace adaptivity styles to another folder

--- frontend/src/pages/ContactsPage.tsx
+++ frontend/src/pages/ContactsPage.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { CONTACTS } from "../common";
 import "animate.css";
 import "../styles/components/contacts.css";
+import "../styles/components/adaptivity/contacts-adaptivity.css"
 
 const ContactsPage: React.FC = () => {
     const [hoveredIcon, setHoveredIcon] = useState<string | null>(null);
