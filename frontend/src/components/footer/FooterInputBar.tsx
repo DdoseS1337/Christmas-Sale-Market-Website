4a33d5ee7e31586c08841d40121c7b717712e377
Pier228 <artem.buxanenko@gmail.com>
2023-11-27 14:54:25 +0200

    Made Subscribe System in FooterInputBar component

--- frontend/src/components/footer/FooterInputBar.tsx
+++ frontend/src/components/footer/FooterInputBar.tsx
@@ -1,23 +1,83 @@
 import RoundedButton from "../common/RoundedButton";
 import "../../styles/components/footer/rounded-input.css";
 import "../../styles/components/footer/search-input.css";
+import { useFormik } from "formik";
 import { classNames } from "primereact/utils";
+import { InputText } from "primereact/inputtext";
+import { Message } from "primereact/message";
+import { useEffect, useRef, useState } from "react";
+import { Toast } from "primereact/toast";
 
 interface IProps {
-	className?: string;
+    className?: string;
 }
 
 const FooterInputBar = ({ className }: IProps) => {
-	return (
-		<div className={classNames("footer-input-bar", className)}>
-			<input
-				className="rounded-input"
-				placeholder="Ваша пошта"
-				type="email"
-			/>
-			<RoundedButton>Підписатися</RoundedButton>
-		</div>
-	);
+    const [send, isSended] = useState(false);
+    const toast = useRef<Toast>(null);
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+        },
+        validate: (values) => {
+            const errors: Record<string, string> = {};
+
+            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+                errors.email = "Введіть коректну електронну пошту";
+            }
+
+            return errors;
+        },
+        onSubmit: () => isSended(true),
+    });
+
+    useEffect(() => {
+        if (send) {
+            toast.current?.show({
+                severity: "success",
+                summary: "Успіх",
+                detail: `Ви успішно підписалися на наші оновлення!`,
+                life: 5000,
+            });
+        }
+    }, [send]);
+
+    return (
+        <>
+            {send && <Toast ref={toast} />}
+            <form
+                onSubmit={formik.handleSubmit}
+                className={classNames("footer-input-bar", className)}
+            >
+                <InputText
+                    unstyled
+                    keyfilter="email"
+                    className={`rounded-input ${
+                        formik.touched.email && formik.errors.email
+                            ? "error"
+                            : ""
+                    }`}
+                    placeholder="Ваша пошта"
+                    type="email"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
+                <RoundedButton onClick={() => formik.handleSubmit()}>
+                    Підписатися
+                </RoundedButton>
+            </form>
+            {formik.touched.email && formik.errors.email ? (
+                <Message
+                    severity="warn"
+                    className="mt-2 d-flex"
+                    text={formik.errors.email}
+                />
+            ) : null}
+        </>
+    );
 };
 
 export default FooterInputBar;
