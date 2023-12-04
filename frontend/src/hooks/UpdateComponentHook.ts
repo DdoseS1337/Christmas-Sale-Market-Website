9bab3f6b71b79a59a159cb525a46a80650ecc952
Pier228 <artem.buxanenko@gmail.com>
2023-12-04 20:10:15 +0200

    Fixed ProductsRowPresenter adaptivity, Added ContinueShopping component

--- frontend/src/hooks/UpdateComponentHook.ts
+++ frontend/src/hooks/UpdateComponentHook.ts
@@ -4,7 +4,6 @@ export const useRefresh = () => {
     const [, setRefresh] = useState<boolean>(true);
     return () => {
         setRefresh(old => {
-            console.log(12355)
             return !old
         });
     };
