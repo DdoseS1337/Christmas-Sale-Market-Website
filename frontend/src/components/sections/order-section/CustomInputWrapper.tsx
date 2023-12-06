8ae68d0e73de79c0b36c90f7e3c4073ba78bf6b6
Pier228 <artem.buxanenko@gmail.com>
2023-12-06 13:41:48 +0200

    manifest fix, footer email input fix

--- frontend/src/components/sections/order-section/CustomInputWrapper.tsx
+++ frontend/src/components/sections/order-section/CustomInputWrapper.tsx
@@ -5,42 +5,42 @@ import { ReactNode, useEffect } from "react";
 import { classNames } from "primereact/utils";
 
 export interface ICustomInputWrapperProps {
-	isInvalid: (field: keyof IOrderCustomerInformation) => boolean;
-	field: keyof IOrderCustomerInformation;
-	label: string;
-	width: "1/3" | "1/2" | "full";
-	errors: FormikErrors<IOrderCustomerInformation>;
-	isRequired?: boolean;
-	input: ReactNode;
+    isInvalid: (field: keyof IOrderCustomerInformation) => boolean;
+    field: keyof IOrderCustomerInformation;
+    label: string;
+    width: "1/3" | "1/2" | "full";
+    errors: FormikErrors<IOrderCustomerInformation>;
+    isRequired?: boolean;
+    input: ReactNode;
 }
 
 export const CustomInputWrapper = ({
-	isInvalid,
-	field,
-	label,
-	width,
-	errors,
-	isRequired,
-	input,
+    isInvalid,
+    field,
+    label,
+    width,
+    errors,
+    isRequired,
+    input,
 }: ICustomInputWrapperProps) => {
-	return (
-		<Form.Group
-			className={classNames("d-flex flex-column gap-2", {
-				"input-w-33": width === "1/3",
-				"input-w-50": width === "1/2",
-				"input-w-full": width === "full",
-			})}
-		>
-			<label htmlFor={field}>
-				{label}
-				{isRequired && "*"}
-			</label>
+    return (
+        <Form.Group
+            className={classNames("d-flex flex-column gap-2", {
+                "input-w-33": width === "1/3",
+                "input-w-50": width === "1/2",
+                "input-w-full": width === "full",
+            })}
+        >
+            <label htmlFor={field}>
+                {label}
+                {isRequired && "*"}
+            </label>
 
-			{input}
+            {input}
 
-			<small className="p-error">
-				{isInvalid(field) && errors[field]}
-			</small>
-		</Form.Group>
-	);
+            <small className="p-error">
+                {isInvalid(field) && errors[field]}
+            </small>
+        </Form.Group>
+    );
 };
