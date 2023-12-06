60831ff83930712c7a23e2a8b3e767af78a41e77
Pier228 <artem.buxanenko@gmail.com>
2023-12-06 12:05:10 +0200

    Added Snowfall adaptivity fix

--- frontend/src/components/ui/Snowfall.tsx
+++ frontend/src/components/ui/Snowfall.tsx
@@ -1,8 +1,12 @@
+import MediaQuery from "react-responsive";
+
 const Snowfall = () => {
     return (
-        <div className="snow_wrap">
-            <div className="snow"></div>
-        </div>
+        <MediaQuery minWidth={1094}>
+            <div className="snow_wrap">
+                <div className="snow"></div>
+            </div>
+        </MediaQuery>
     );
 };
 
